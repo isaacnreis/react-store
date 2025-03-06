d120d084a32ef094fcd1fd379f1bf490dd11b390
Isaac N Reis <isaac.reis@teknisa.com>
2025-03-06 20:41:48 -0300

    Corrigindo API do Stripe no deploy

--- src/components/CheckoutButton/CheckoutButton.tsx
+++ src/components/CheckoutButton/CheckoutButton.tsx
@@ -4,19 +4,26 @@ const CheckoutButton = () => {
   const { cart } = useCartStore();
 
   const handleCheckout = async () => {
-    const response = await fetch("/api/checkout", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ cartItems: cart }),
-    });
+    const response = await fetch(
+      "https://react-store-lyart.vercel.app/api/checkout",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cartItems: cart }),
+      }
+    );
 
     const data = await response.json();
     if (data.id) {
-      window.location.href = `https://checkout.stripe.com/pay/${data.id}`;
+      window.location.href = data.url;
     }
   };
 
-  return <button onClick={handleCheckout}>Pagar com Stripe</button>;
+  return (
+    <button type="button" onClick={handleCheckout}>
+      Pagar com Stripe
+    </button>
+  );
 };
 
 export default CheckoutButton;
